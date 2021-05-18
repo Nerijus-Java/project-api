05d28487f614fdc7ea4ba1b702879be9401a2bf6
Nerijus Viliusis <pcneriyus3@gmail.com>
2021-05-18 16:19:13 +0300

    Initial commit

---
+++ src/test/java/lt/codeacademy/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+//package lt.codeacademy.project;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ProjectApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
