4431b33e871a7bef6517624159ecfd0847a9c27d
Nerijus Viliusis <pcneriyus3@gmail.com>
2021-06-01 22:38:56 +0300

    clean up

--- src/main/java/lt/codeacademy/project/api/config/SecurityConfig.java
+++ src/main/java/lt/codeacademy/project/api/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package lt.codeacademy.project.api.config;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
