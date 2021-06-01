071bb4b33caab2cb6e95ce1e3ccc3f03a97f8884
Nerijus Viliusis <pcneriyus3@gmail.com>
2021-06-01 22:39:48 +0300

    Annotation and public to protected changed

--- src/main/java/lt/codeacademy/project/api/config/SecurityConfig.java
+++ src/main/java/lt/codeacademy/project/api/config/SecurityConfig.java
@@ -1,16 +1,17 @@
 package lt.codeacademy.project.api.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
+@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
-    public void configure(HttpSecurity http) throws Exception {
-
+    protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
