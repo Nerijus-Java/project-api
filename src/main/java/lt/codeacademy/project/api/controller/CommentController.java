fef0a342da8a6938e75ce4d28dcc8691652d17be
Nerijus Viliusis <pcneriyus3@gmail.com>
2021-06-08 21:25:18 +0300

    All crud methods work in Comments

--- src/main/java/lt/codeacademy/project/api/controller/CommentController.java
+++ src/main/java/lt/codeacademy/project/api/controller/CommentController.java
@@ -3,12 +3,15 @@ package lt.codeacademy.project.api.controller;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lt.codeacademy.project.api.EndPoint;
+import lt.codeacademy.project.api.dto.CommentDto;
 import lt.codeacademy.project.api.entity.Comment;
+import lt.codeacademy.project.api.entity.User;
 import lt.codeacademy.project.api.service.CommentsService;
 import lt.codeacademy.project.api.service.PostService;
+import lt.codeacademy.project.api.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -22,35 +25,36 @@ public class CommentController {
 
     private final CommentsService commentsService;
     private final PostService postService;
+    private final UserService userService;
+    private final CommentDto commentDto;
 
-    public CommentController(CommentsService commentsService, PostService postService) {
+    public CommentController(CommentsService commentsService, PostService postService, UserService userService) {
         this.commentsService = commentsService;
         this.postService = postService;
+        this.userService = userService;
+        this.commentDto = new CommentDto();
     }
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     @ApiOperation(value = "Get all Comments", httpMethod = "GET")
-    public List<Comment> getComments() {
-        return commentsService.getAllComments();
+    public List<CommentDto> getComments() {
+        return commentDto.parseList(commentsService.getAllComments());
     }
 
     @GetMapping(value = EndPoint.BY_UUID, produces = MediaType.APPLICATION_JSON_VALUE)
     @ApiOperation(value = "Get Comment by UUID", httpMethod = "GET")
-    public Comment getComment(@PathVariable(EndPoint.UUID) UUID uuid) {
-        return commentsService.getComment(uuid);
+    public CommentDto getComment(@PathVariable(EndPoint.UUID) UUID uuid) {
+        return commentDto.parseObject(commentsService.getComment(uuid));
     }
-
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE , value = EndPoint.BY_POST_UUID)
     @ApiOperation(value = "Create comment", httpMethod = "POST")
     @ResponseStatus(HttpStatus.CREATED)
-    public void createComment(@Valid @RequestBody Comment comment, @PathVariable(EndPoint.POST_UUID) UUID id) {
-        //TODO set user to comments
+    public void createComment(@Valid @RequestBody Comment comment, @PathVariable(EndPoint.POST_UUID) UUID id,@AuthenticationPrincipal String username) {
         comment.setPost(postService.getPost(id));
-//        comment.setUser();
+        comment.setUser((User) userService.loadUserByUsername(username));
         commentsService.addComment(comment);
     }
-
 
     @DeleteMapping(value = EndPoint.BY_UUID)
     @ApiOperation(value = "Remove Comment", httpMethod = "DELETE")
@@ -59,10 +63,10 @@ public class CommentController {
         commentsService.removeComment(id);
     }
 
-
     @PutMapping
     @ApiOperation(value = "Update Comment", httpMethod = "PUT")
-    public Comment updateComment(@Valid @RequestBody Comment comment) {
-        return commentsService.updateComment(comment);
+    public CommentDto updateComment(@Valid @RequestBody Comment comment) {
+        comment.setUser(commentsService.getComment(comment.getId()).getUser());
+        return commentDto.parseObject(commentsService.updateComment(comment));
     }
 }
---
+++ src/main/java/lt/codeacademy/project/api/dto/CommentDto.java
@@ -0,0 +1,27 @@
+package lt.codeacademy.project.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lt.codeacademy.project.api.entity.Comment;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommentDto {
+
+    private String id;
+    private String description;
+    private String username;
+
+    public List<CommentDto> parseList(List<Comment> comments){
+        return comments.stream().map(e -> parseObject(e)).collect(Collectors.toList());
+    }
+
+    public CommentDto parseObject(Comment comment){
+        return new CommentDto(comment.getId().toString(),comment.getDescription(),comment.getUser().getUsername());
+    }
+}
